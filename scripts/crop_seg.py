53906b69ec965632c377b418047f2735bf1bfadf
Margot Belot <margotbelotmfn@gmail.com>
2023-01-19 09:20:32 +0000

    Update crop_seg.py

--- scripts/crop_seg.py
+++ scripts/crop_seg.py
@@ -30,7 +30,7 @@ CLS_AMOUNT = 3
 def parsing_args():
     '''generate the command line arguments using argparse'''
     #create_crops.py [-h] -f <file> -d <dir>
-    usage = 'crop_seg.py [-h] [-c N] -o /path/to/jpgs_outputs -j </path/to/jpgs> -m </path/to/model> '
+    usage = 'crop_seg.py [-h] [-c N] -m <model/number> -c <class/number> -j </path/to/jpgs> -o </path/to/jpgs_outputs> ' #new
     parser =  argparse.ArgumentParser(description=__doc__,
             add_help = False,
             usage = usage
@@ -72,10 +72,13 @@ def parsing_args():
             help='Directory where the jpgs are stored.'
             )
     
-    parser.add_argument(
+    parser.add_argument( #new
             '-m', '--model',
             metavar='',
-            type=str,
+            choices = range(1,4),
+            type=int,
+            default = 1,
+            action='store',
             required = True,
             help='Path to the model to be used.'
             )
@@ -86,6 +89,29 @@ def parsing_args():
 
     return args
 
+def get_modelnum(model_int): #new
+    """
+    Returns the chosen model.
+
+    Args:
+         model_int (int): integer for model selection.
+
+    Returns:
+         path (str): path to the selected model.
+    """
+    script_dir = os.path.dirname(__file__)
+    rel_path1 = "models/model_labels_box.pth"
+    model_file1 = os.path.join(script_dir, rel_path1)
+    rel_path2 = "models/model_labels_class.pth"
+    model_file2 = os.path.join(script_dir, rel_path2)
+    rel_path3 = "models/model_labels_h_t.pth"
+    model_file3 = os.path.join(script_dir, rel_path3)
+    if model_int == 1:
+        return model_file1
+    elif model_int == 2:
+        return model_file2
+    else:
+        return model_file3
 
 def get_classtype(class_int: int) -> list:
     """
@@ -108,7 +134,7 @@ def get_classtype(class_int: int) -> list:
 # does not execute main if the script is imported as a module
 if __name__ == '__main__': 
     args = parsing_args()
-    model_path = args.model
+    model_path = get_modelnum(args.model) #new
     jpeg_dir = args.jpg_dir
     classes = get_classtype(args.classes)
     out_dir = args.out_dir
