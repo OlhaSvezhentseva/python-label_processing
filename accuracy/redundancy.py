9a1acf304dc4d0fdd743c6fb9658c48eca6b6fe8
Margot Belot <margotbelotmfn@gmail.com>
2023-04-14 12:29:08 +0000

    Update redundancy.py

--- accuracy/redundancy.py
+++ accuracy/redundancy.py
@@ -1,29 +1,33 @@
 # Import librairies
 import pandas as pd
 import re
+import warnings
+warnings.filterwarnings('ignore')
 
-PATH = "/Users/Margot/Desktop/ground_truth.csv"
+
+#PATH = "/Users/Margot/Desktop/Golden_Dataset.csv"
 
 df = pd.read_csv(PATH, sep= ";")
 
 def clean_data(df):
-	df['text'] = df['text'].str.lower() #remove lowercase
-	df['text'] = df['text'].str.replace('[^\w\s]','') #remove punctuation
-	df['text'] = df['text'].str.replace(' ', '') #remove whitespace
-	df['text'] = df['text'].str.replace(r'httpcollmfnberlindeu([0-9a-zA-Z]+)', 'httpcollmfnberlindeu') #remove NURIs numbers
-	df['text'] = df['text'].str.replace('\d+', '') #remove digits
-	return df
+    df['text'] = df['text'].str.lower() #remove lowercase
+    df['text'] = df['text'].str.replace('[^\w\s]','') #remove punctuation
+    df['text'] = df['text'].str.replace(' ', '') #remove whitespace
+    df['text'] = df['text'].str.replace(r'httpcollmfnberlindeu([0-9a-zA-Z]+)', 'httpcollmfnberlindeu') #remove NURIs numbers - completly?
+    df['text'] = df['text'].str.replace('\d+', '') #remove digits? y/n
+    return df
 
 def redundancy(df):
-	df = clean_data(df)
-	duplicates = df["text"]
-	df[duplicates.isin(duplicates[duplicates.duplicated()])].sort_values("text") #groupby duplicates
-	df = pd.concat(g for _, g in df.groupby("text") if len(g) > 1)
-	return df
+    df = clean_data(df)
+    duplicates = df["text"]
+    df[duplicates.isin(duplicates[duplicates.duplicated()])].sort_values("text") #groupby duplicates
+    df = pd.concat(g for _, g in df.groupby("text") if len(g) > 1)
+    return df
 
-def sum_redundancy(df):
+def per_redundancy(df):
+    df_clean = df.copy()
     df = redundancy(df)
-    result = df["text"].duplicated().sum() #find sum of duplicates
-    return print("Redundant text:", result)
-
-    
+    sum_text = df_clean["text"].value_counts().sum()
+    sum_dup = df["text"].duplicated().sum() #find sum of duplicates
+    percentage_red = round(sum_dup/sum_text*100)
+    return print("Percentage redundant text:", "%s%%"%percentage_red)
