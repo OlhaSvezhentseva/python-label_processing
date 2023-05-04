8d6280fb171dfc6eaf880ad3825e57260affcafa
Margot Belot <margotbelotmfn@gmail.com>
2023-05-04 11:02:34 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='label_processing',
     version='1.1',
     description='Package for label processing',
-    py_modules=["redundancy", "segmentation_cropping", "text_recognition", "vision", "utils"],
+    py_modules=["redundancy", "segmentation_cropping", "text_recognition", "utils", "vision", "cluster_ID"],
     package_dir={'': 'label_processing'},
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -19,7 +19,8 @@ setup(
     scripts=["scripts/crop_seg.py",
              "scripts/tesseract_ocr.py",
              "scripts/vision_api.py",
-             "scripts/label_redundancy.py"],
+             "scripts/label_redundancy.py",
+             "scripts/clustering_preprocessing.py"],
     #include_package_data=True, #include the static data specified in the MANIFEST.in
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -35,7 +36,6 @@ setup(
         "tesseract",
         "pillow",
         "google-cloud",
-        "deskew",
-        "google"
+        "deskew"
     ],
 )
