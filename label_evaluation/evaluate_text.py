9a8f5793397e58a0be6ad65877989386be6ac780
Olha Svezhnetseva <svezhentseva@uni-potsdam.de>
2023-08-25 18:41:45 +0200

    Restore doc strings

--- label_evaluation/evaluate_text.py
+++ label_evaluation/evaluate_text.py
@@ -1,27 +1,44 @@
+#Import Librairies
 import jiwer
 import json
 import numpy as np
 import csv
 import os
-import argparse
 from cer import calculate_cer
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import label_processing.utils as utils
+from pathlib import Path
+import warnings
+import argparse
+warnings.filterwarnings('ignore')
 
 
-def get_predicted_transcriptions(filename):
-    """This function loads predictions from a json file."""
-    f = open(filename)
-    transcriptions = json.load(f)
+def get_predicted_transcriptions(filename: str) -> str:
+    """
+    Loads predictions from the OCR outputs as a json file.
+
+        Args:
+            filename (str): path to the json file
+
+        Returns:
+            transcriptions (str): loaded json file
+    """
+    with open(filename) as f:
+        transcriptions = json.load(f)
     return transcriptions
 
 
-def get_gold_transcriptions(filename):
+def get_gold_transcriptions(filename: str) -> dict:
     """
-    This function loads gold transcriptions from a csv file.
-    :param filename: filename as a string
-    :return: dictionary of the form {'ID': text, }
+    Loads predictions from the groundtruth transcriptions as a csv file.
+
+    Args:
+        filename (str): path to the groundtruth csv
+
+    Returns:
+        gold_transcriptions (dict): dictionary of the form {'ID': text}
     """
     gold_transcriptions = {}
     with open(filename, encoding='utf-8-sig') as file_in:
@@ -34,31 +51,41 @@ def get_gold_transcriptions(filename):
     return gold_transcriptions
 
 
-def calculate_scores(gold_text, predicted_text):
+def calculate_scores(gold_text: str, predicted_text: str) -> tuple:
     """
-    This function calculates CER and WER comparing 2 strings.
-    :param gold_text: gold transcription as a string
-    :param predicted_text:  predicted transcription as a string
-    :return: tuple of 2 scores/None
+    Calculates CER and WER by comparing the predicted and groundtruth transcriptions.
+
+    Args:
+        gold_text (str): groundtruth transcription as a string
+        predicted_text (str): predicted transcription as a string
+
+    Returns:
+        wer, cer (tuple): tuple of the two scores/None
     """
-    # Ignore NURIs
     if not gold_text.startswith("http") and not gold_text.startswith("MfN URI"):
         all_scores = jiwer.compute_measures(gold_text, predicted_text)
         # Calculate normalized WER
-        wer = (all_scores["insertions"] + all_scores["deletions"] + all_scores["substitutions"])/len(gold_text)
+        wer = ((all_scores["insertions"] + all_scores["deletions"] +
+            all_scores["substitutions"])/len(gold_text))
         # Calculate normalized CER
         cer = calculate_cer([gold_text], [predicted_text])
+        wer = round(wer, 2)
+        cer = round(cer, 2)
         return wer, cer
     return None
 
 
-def compare_transcriptions(gold_transcriptions, ocr_transcriptions, file_name):
+def compare_transcriptions(gold_transcriptions: dict, ocr_transcriptions: list, file_name: str) -> tuple:
     """
-    This function writes evaluation results into a csv table.
-    :param gold_transcriptions: ground truth data as a dictionary
-    :param ocr_transcriptions: predicted transcriptions as a list of dicts
-    :param file_name: the name of a CSV file which will be created
-    :return: tuple of 2 lists with scores
+    Writes evaluation results into a csv table.
+
+    Args:
+        gold_transcriptions (dict): groundtruth data as a dictionary
+        ocr_transcriptions (list): predicted transcriptions as a list of dicts
+        file_name (str): the name of a CSV file which will be created
+
+    Returns:
+        all_wers, all_cers (tuple): tuple of two lists with scores
     """
     all_wers = []
     all_cers = []
@@ -77,13 +104,14 @@ def compare_transcriptions(gold_transcriptions, ocr_transcriptions, file_name):
     return all_wers, all_cers
 
 
-def create_plot(data, name, file_name):
+def create_plot(data: list, name: list, file_name: str) -> None:
     """
-    This function creates a violin plot and saves it.
-    :param data: scores as a list
-    :param name: name of the future plot
-    :param file_name: the name of the file the plot will be saved in
-    :return: None
+    Create violin plots for the CER and WER scores respectively.
+
+    Args:
+        data (list): scores as a list
+        name (list): name of the future plot
+        file_name (str): the name of the file the plot will be saved in
     """
     plot = pd.DataFrame(data, columns=[name])
     sns.violinplot(data=plot[name], cut=1.0).set(title=name)
@@ -92,30 +120,24 @@ def create_plot(data, name, file_name):
     print(f"Plot saved in {file_name}")
 
 
-def evaluate_text_predictions(ground_truth_file, predictions_file, result_folder):
+def evaluate_text_predictions(ground_truth_file: str, predictions_file: str, out_dir) -> tuple:
     """
-    This function evaluates OCR predictions
-    :param ground_truth_file: ground truth data as a CSV
-    :param predictions_file:  OCR output in json
-    :param result_folder: name of the folder the evaluation result will be saved in
-    :return: None
-    """
+    Evaluates OCR predictions.
 
+    Args:
+        ground_truth_file (str): path to groundtruth data as a CSV
+        predictions_file (str): path to OCR output as a json file
+
+    Returns:
+        wers, cers (tuple): tuple of two lists with scores
+    """
     ground_truth = get_gold_transcriptions(ground_truth_file)
     generated_transcriptions = get_predicted_transcriptions(predictions_file)
-
-    # create result_folder
-    # parent_dir = "text_evaluation"
-    # path = os.path.join(parent_dir, result_folder)
-    path = os.path.join(result_folder)
-    if not os.path.exists(path):
-        os.mkdir(path)
-    wers, cers = compare_transcriptions(ground_truth, generated_transcriptions, f"{path}/ocr_evaluation.csv")
+    wers, cers = compare_transcriptions(ground_truth, generated_transcriptions, f"{out_dir}/ocr_evaluation.csv")
     print(f"Mean CER: {round(np.mean(cers), 2)}, Mean WER: {round(np.mean(wers), 2)}")
-    create_plot(cers, "CERs", f"{path}/cers.png")
-    create_plot(wers, "WERs", f"{path}/wers.png")
+    create_plot(cers, "CERs", f"{out_dir}/cers.png")
+    create_plot(wers, "WERs", f"{out_dir}/wers.png")
     return
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
