39f67f10f0586080c37d62b0cc4f67948e7593f3
Belot, Margot <margot.belot@mfn.berlin>
2023-09-03 06:11:41 +0000

    Update scripts/cluster_visualisation.py

--- scripts/cluster_visualisation.py
+++ scripts/cluster_visualisation.py
@@ -17,6 +17,9 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 
+import warnings
+warnings.filterwarnings('ignore')
+
 def parsing_args() -> argparse.ArgumentParser:
     '''generate the command line arguments using argparse'''
     usage = 'cluster_visualisation.py [-h] [-c N] \
@@ -36,7 +39,7 @@ def parsing_args() -> argparse.ArgumentParser:
             metavar='',
             type=str,
             default = os.getcwd(),
-            help=('Directory in which the resulting plot will be stored.\n'
+            help=('Directory in which the resulting crops and the csv will be stored.\n'
                   'Default is the user current working directory.')
             ),
     parser.add_argument(
@@ -44,80 +47,96 @@ def parsing_args() -> argparse.ArgumentParser:
             metavar='',
             type=str,
             required=True,
-            help=('Path to cluster_json')
+            help=('path to cluster_json')
             ),
     parser.add_argument(
             '-gt', '--ground_truth',
             metavar='',
             type=str,
             required=True,
-            help=('Path to ground truth json')
+            help=('path to ground truth json')
             )
     args = parser.parse_args()
 
     return args
     
 
-def is_word(token: str) -> bool:
+def is_word(token: str):
     """
-    Checks if a token (str) is a word or not
+    Check if a token is a valid word.
 
     Args:
-        token (str): token from word_tokenize
+        token (str): The token to check.
 
     Returns:
-        bool: true if word and false if not
+        bool: True if the token is a valid word, False otherwise.
     """
     if token not in string.punctuation and not token.isspace():
         if len(token) >= 3:
             return True 
-    return False
 
-def build_word_vectors(labels: list[dict[str, str]]) -> tuple(Word2Vec,
-                                                              list[dict[str, str]]):
+def build_word_vectors(labels):
     """
-    The function builds a vector for each word in the label.
+    Build word vectors for labels.
+
     Args:
-        labels (list[dict[str, str]]): labels from the json file
+        labels (list): List of label objects containing text.
+
     Returns:
-        tuple(Word2Vec,list[dict[str, str]]): tuple of model and tokenized labels
+        gensim.models.Word2Vec: Word2Vec model trained on label tokens.
+        list: List of tokenized labels with associated IDs.
     """
     tokenized_labels = []
     for label in labels:
         tokens = [token.lower() for token in word_tokenize(label["text"]) if is_word(token)]
         tokenized_label = {"ID": label["ID"], "tokens": tokens}
         tokenized_labels.append(tokenized_label)
-    model = Word2Vec([label["tokens"] for label in tokenized_labels], min_count = 1, vector_size = 100,
+    model = gensim.models.Word2Vec([label["tokens"] for label in tokenized_labels], min_count = 1, vector_size = 100,
                                              window = 2, sg = 1)
-    return model,  tokenized_labels
+    return model, tokenized_labels
 
 
-def build_mean_label_vector(model: Word2Vec, labels: list[dict[str, str]]):
+def build_mean_label_vector(model: gensim.models.Word2Vec, labels: list):
     """
-    The function builds a vector for a label (by taking the mean of word vectors).
+    Build a vector for a label by taking the mean of word vectors.
 
     Args:
-        model (Word2Vec): Word2Vec model
-        labels (list[dict[str, str]]): #TODO
+        model (gensim.models.Word2Vec): Word2Vec model.
+        labels (list): List of tokenized labels with associated IDs.
 
     Returns:
-        list[dict[str, str]]): #TODO
+        dict: Dictionary mapping label IDs to their mean vectors.
     """
     labels_vectors = {}
     for label in labels:
+        # np.mean([model.wv[token] for token in label["tokens"]])
         mean_vector = np.mean([model.wv[token] for token in label["tokens"]], axis=0)
         labels_vectors[label["ID"]] = mean_vector
     return labels_vectors
 
-def load_json(filepath: str) -> list[dict[str, str]]:
+def load_json(file: str):
     """
-    Loads json file
+    Load data from a JSON file.
+
+    Args:
+        file (str): Path to the JSON file.
+
+    Returns:
+        dict: Loaded JSON data.
     """
-    with open(filepath, 'r') as f:
+    with open(file, 'r') as f:
         data = json.load(f)
     return data
 
 def main(ground_truth_json: str, cluster_json: str, out_dir: str):
+    """
+    Main function for processing label data, performing T-SNE dimensionality reduction, and saving a scatter plot.
+
+    Args:
+        ground_truth_json (str): Path to the ground truth JSON file.
+        cluster_json (str): Path to the cluster JSON file.
+        out_dir (str): Directory where the scatter plot image will be saved.
+    """
     labels_file = ground_truth_json
     labels = load_json(labels_file)
     model1, tokens = build_word_vectors(labels)
@@ -133,18 +152,19 @@ def main(ground_truth_json: str, cluster_json: str, out_dir: str):
     df['tsne-2d-two'] = tsne_results[:,1]
     df['y'] = clusters_sorted 
     
-    plt.figure(figsize=(16,10))
+    plt.figure(figsize=(20, 12))  # Increase the figure size
     sns.scatterplot(
         x="tsne-2d-one", y="tsne-2d-two",
         hue="y",
-        style = "y",
+        style="y",
         palette=sns.color_palette("hls", len(set(df.y))),
         data=df,
         legend="full",
         alpha=0.9
     ).set(title="Label data T-SNE projection")
-    plt.legend(loc = 'upper right', bbox_to_anchor=(1.15, 1.5), ncol=1)
-    plt.savefig(os.path.join(out_dir, "cluster_plot.png"))
+    plt.legend(loc='upper right', bbox_to_anchor=(1.15, 1.5), ncol=1)
+    plt.tight_layout()  # Adjust margins
+    plt.savefig(os.path.join(out_dir, "cluster_plot.png"), bbox_inches='tight')  # Save with bbox_inches='tight'
     return print(f"\nThe image has been successfully saved in {out_dir}")
 
     
