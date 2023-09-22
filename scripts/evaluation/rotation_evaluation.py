818746190fd974c8274740ccd3f93cee43578724
Belot, Margot <margot.belot@mfn.berlin>
2023-09-22 16:28:17 +0000

    correct file - comments

--- scripts/evaluation/rotation_evaluation.py
+++ scripts/evaluation/rotation_evaluation.py
@@ -13,34 +13,17 @@ def rotation_evaluation(input_csv_path, output_folder):
         input_csv_path (str): Path to the input CSV file containing data.
         output_folder (str): Path to the folder where the plot and value counts will be saved.
     """
-    # Read the CSV file
     df = pd.read_csv(input_csv_path, sep=';')
-
-    # Group and count values
     value_counts = df.groupby(['before'])['pred'].value_counts()
-
-    # Define conditions and choices
     conditions = [(df['pred'] == "straight"),
                  (df['pred'] == "not_straight")]
     choices = ['match', 'no_match']
-
-    # Create a new column in DataFrame that displays results of comparisons
     df['comparison_values'] = np.select(conditions, choices, default='Tie')
-
-    # Create a comparison plot
     sns.set_theme(style="whitegrid")
     plot = sns.displot(df, x="pred", hue="comparison_values")
-
-    # Define the output plot file path
     output_plot_path = os.path.join(output_folder, "comparison_plot.png")
-
-    # Save the plot to the target folder
     plot.savefig(output_plot_path)
-
-    # Define the output text file path
     output_text_path = os.path.join(output_folder, "value_counts.txt")
-
-    # Save the value_counts to a text file
     with open(output_text_path, 'w') as text_file:
         text_file.write(str(value_counts))
 
