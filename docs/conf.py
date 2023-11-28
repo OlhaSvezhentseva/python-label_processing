4bbc04df6d9dbdc2bb0d4e278d87477702203242
Belot, Margot <margot.belot@mfn.berlin>
2023-11-28 13:37:32 +0100

    solving warning make html

--- docs/conf.py
+++ docs/conf.py
@@ -24,8 +24,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'renku'
 html_static_path = ['_static']
+autodoc_mock_imports = ['google.cloud']
 
 import sys, os
 
