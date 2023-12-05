e5c5144eda28d8b737753f3e9eb05448d3a3ed20
Belot, Margot <margot.belot@mfn.berlin>
2023-12-05 14:51:06 +0100

    added doctsrings to test text recognition

--- tests/label_processing_tests/test_text_recognition.py
+++ tests/label_processing_tests/test_text_recognition.py
@@ -11,6 +11,10 @@ from label_processing.text_recognition import ImageProcessor, Tesseract, Threshm
 class TestImageProcessor(unittest.TestCase):
     """
     A test suite for the ImageProcessor class.
+
+    Attributes:
+        image_path (Path): The file path to a sample image for testing.
+        image (numpy.ndarray): The image loaded using OpenCV for testing.
     """
     image_path: Path =  Path("../testdata/cropped_pictures/coll.mfn-berlin.de_u_115ff7__Preview_label_typed_1.jpg")
     image = cv2.imread(str(image_path))
@@ -49,15 +53,21 @@ class TestImageProcessor(unittest.TestCase):
         
     def test_different_pictures(self):
         """
-        test if the preprocessed picture is different from the original picture
+        Test if the preprocessed picture is different from the original picture.
+
+        Checks if the preprocessed image is not close to the original image.
         """
         preprocessor = ImageProcessor.read_image(self.image_path)
         preprocessor = preprocessor.preprocessing(Threshmode.OTSU)
         if self.image.shape == preprocessor.image.shape:
             self.assertFalse(np.allclose(self.image, preprocessor.image))
 
-    
     def test_save_image(self):
+        """
+        Test the save_image method of ImageProcessor.
+
+        Checks if the save_image method correctly saves the image to the specified path.
+        """
         preprocessor = ImageProcessor.read_image(self.image_path)
         path = "../testdata/output"
         expected_path = Path(f"{path}/{self.image_path.name}")
@@ -65,26 +75,55 @@ class TestImageProcessor(unittest.TestCase):
         self.assertTrue(expected_path.exists())
     
     def test_qr_code_reader(self):
+        """
+        Test the qr_code_reader method of ImageProcessor.
+
+        Checks if the qr_code_reader method correctly reads a QR code from the image.
+        """
         preprocessor = ImageProcessor.read_image(self.image_path)
         value = preprocessor.read_qr_code()
         self.assertTrue(value == None or isinstance(value, str))
     
 
 class TestTesseract(unittest.TestCase):
+    """
+    A test suite for the Tesseract class.
+
+    Attributes:
+        image_path (Path): The file path to a sample image for testing.
+        image (numpy.ndarray): The image loaded using OpenCV for testing.
+    """
     image_path: Path =  Path("../testdata/cropped_pictures/coll.mfn-berlin.de_u_115ff7__Preview_label_typed_1.jpg")
     image = cv2.imread(str(image_path))
     
     def test_constructor_no_image(self):
+        """
+        Test the constructor of Tesseract without providing an image.
+
+        Checks if the Tesseract instance is correctly constructed without an image,
+        and then successfully assigns an image.
+        """
         tesseract_wrapper = Tesseract()
         self.assertIsNone(tesseract_wrapper.image)
         tesseract_wrapper.image = self.image
         self.assertIsInstance(tesseract_wrapper.image, np.ndarray)
     
     def test_constructor_image(self):
+        """
+        Test the constructor of Tesseract with an image provided.
+
+        Checks if the Tesseract instance is correctly constructed with the provided image.
+        """
         tesseract_wrapper = Tesseract(image = self.image)
         self.assertIsInstance(tesseract_wrapper.image, np.ndarray)
     
     def test_image_to_string(self):
+        """
+        Test the image_to_string method of Tesseract.
+
+        Checks if the image_to_string method correctly extracts text from an image
+        processed by the ImageProcessor.
+        """
         preprocessor = ImageProcessor(self.image, self.image_path)
         tesseract_wrapper = Tesseract(image = preprocessor)
         result = tesseract_wrapper.image_to_string()
