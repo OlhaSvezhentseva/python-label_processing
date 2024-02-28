c5d8f586ceecfaa8b8167f9e2e8903d6ec17d5e5
Belot, Margot <margot.belot@mfn.berlin>
2024-02-28 15:13:09 +0100

    new test rotator

--- tests/label_processing_tests/test_rotator.py
+++ tests/label_processing_tests/test_rotator.py
@@ -1,28 +1,23 @@
-# Import third-party libraries
 import unittest
-import torchvision
-import cv2
-import numpy as np
+import os
 
 # Import the necessary module from the 'label_processing' module package
-from old.modules.rotator import *
+from label_processing.label_rotation_module import *
 
-class RotatorTests(unittest.TestCase):
-    """
-    A test suite for the Rotator module.
-    """
-    image_path: Path =  Path("../testdata/cropped_pictures/coll.mfn-berlin.de_u_115ff7__Preview_label_typed_1.jpg")
-    image = cv2.imread(str(image_path))
-    
-    
-    def test_rotation_detetctor(self):
-        """
-        Test the rotation detector with an EfficientNet model.
+class TestRotateImage(unittest.TestCase):
+    def test_rotate_image(self):
+        img_path = "/Users/Margot/Desktop/test/d2d306bf-449f-4af8-8c12-b73dba69e37b_label_front_0001_label.jpg"
+        output_dir = '/Users/Margot/Desktop/output'
+        angle = 1 
+        
+        # Call the function to test
+        rotate_image(img_path, angle, output_dir)
+        
+        # Construct the rotated image path
+        rotated_img_path = os.path.join(output_dir, os.path.basename(img_path))
+        
+        # Check if the rotated image is created in the output directory
+        self.assertTrue(os.path.exists(rotated_img_path), "Rotated image file not found")
 
-        This test checks if the RotationDetector class correctly rotates an image using an EfficientNet model.
-        """
-        efficientnet = torchvision.models.efficientnet_b0()
-        rot_det = RotationDetector(efficientnet)
-        rotated_img = rotation(rot_det, self.image, TorchConfig)
-        self.assertIsInstance(rotated_img, np.ndarray)
-    
+if __name__ == '__main__':
+    unittest.main()
